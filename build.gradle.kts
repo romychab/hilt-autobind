ce71e5f14e8477c789df5446caafb7356c89460a
Roman Andrushchenko <rom.andrushchenko@gmail.com>
2026-03-27 17:23:47 +0200

    chore: Setup detekt and ksp testing libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,19 @@ plugins {
     alias(libs.plugins.hilt.android) apply false
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.maven.publish) apply false
+    alias(libs.plugins.dokka) apply false
     alias(libs.plugins.compose) apply false
     alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.detekt)
+}
+
+detekt {
+    config.setFrom(files("$rootDir/detekt.yml"))
+    buildUponDefaultConfig = true
+    source.setFrom(
+        "lib-core/src/main/java",
+        "lib-compiler/src/main/java",
+        "app-example/src/main/java",
+        "app-example/src/main/kotlin",
+    )
 }
--- lib-compiler/build.gradle.kts
+++ lib-compiler/build.gradle.kts
@@ -1,10 +1,14 @@
 plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.maven.publish)
+    alias(libs.plugins.dokka)
 }
 
 kotlin {
     jvmToolchain(17)
+    compilerOptions {
+        freeCompilerArgs.add("-opt-in=org.jetbrains.kotlin.compiler.plugin.ExperimentalCompilerApi")
+    }
 }
 
 mavenPublishing {
@@ -18,4 +22,13 @@ dependencies {
     implementation(libs.ksp.api)
     implementation(libs.kotlin.poet)
     implementation(libs.kotlin.poet.ksp)
+
+    testImplementation(libs.kctfork.core)
+    testImplementation(libs.kctfork.ksp)
+    testImplementation(libs.junit.jupiter)
+    testRuntimeOnly(libs.junit.launcher)
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
 }
